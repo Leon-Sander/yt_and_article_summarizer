f2487c6cb4eda767e28ede32aafd41b3ccd3fc11
Leon Sander <72946124+Leon-Sander@users.noreply.github.com>
2023-08-16 14:05:44 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import utils
 from datetime import datetime
 from summarizer_agent import summary_agent
 
-st.title("Simple Langchain Chatapp :bird:")
+st.title("Youtube and Article Summarizer :bird:")
 tab1, tab2 = st.tabs(["Chat", "Usage Chart"])
 
 if "llm_chain" not in st.session_state:
@@ -24,7 +24,7 @@ def restore_history_messages():
 
 user_message = st.chat_input(placeholder="Type a message...")
 with tab1:
-    st.header("OpenAI  Chatapp")
+    st.header("Summarizer")
     if user_message:
         restore_history_messages()
         output = st.session_state.llm_chain.summarize(query=user_message)
@@ -34,7 +34,7 @@ with tab1:
 
 
 with tab2:
-    st.header("OpenAI Usage Cost")
+    st.header("OpenAI Usage Chart")
     df = pd.read_csv("openai_api_usage.csv")
     st.bar_chart(df, x="us_date_format", y="total_cost_usd")
 
